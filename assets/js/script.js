2eff5e05274b706c7291dfec765ba68b672471a4
Jason <jasontorrealba199@gmail.com>
2023-07-03 11:13:04 -0400

    Added JavaScript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//Call varibales
+var currentDayEl = $('#currentDay');
+var saveButton = $('.saveBtn');
+
+//Sets the current hour
+var hour = dayjs().hour();
+
+//Displays the current day of the week, month, and number day
+function displayDay() {
+  var rightNow = dayjs().format('dddd, MMM DD');
+  currentDayEl.text(rightNow);
+}
+
+//Displays whether each time block is past, present, or future and will set a certain color to the corresponding time block
+function hourofDay() {
+  $('.time-block').each(function () {
+   var eachHour = parseInt($(this).attr('id'));
+   
+   if (eachHour < hour) {
+    $(this).addClass("past");
+   }
+   else if (eachHour === hour) {
+    $(this).addClass("present");
+   }
+   else {
+    $(this).addClass("future");
+   }
+  })
+}
+
+//When you clock the save icon it will store it in the localstorage
+$('.saveBtn').on('click', function () {
+  var key = $(this).siblings('.description').val();
+  var value = $(this).parent().attr('id');
+
+  localStorage.setItem(value, key);
 });
+
+//When you refresh the page the events will persist
+$('#9am .description').val(localStorage.getItem('9am'));
+$('#10am .description').val(localStorage.getItem('10am'));
+$('#11am .description').val(localStorage.getItem('11am'));
+$('#12pm .description').val(localStorage.getItem('12pm'));
+$('#13pm .description').val(localStorage.getItem('13pm'));
+$('#14pm .description').val(localStorage.getItem('14pm'));
+$('#15pm .description').val(localStorage.getItem('15pm'));
+$('#16pm .description').val(localStorage.getItem('16pm'));
+$('#17pm .description').val(localStorage.getItem('17pm'));
+
+//Calls function
+displayDay();
+hourofDay();
